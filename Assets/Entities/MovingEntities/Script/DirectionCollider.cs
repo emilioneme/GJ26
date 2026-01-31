6e2053e391e8122b03e3a0c4ada87830dca6c14c
emilioneme <emilioneme22@gmail.com>
2026-01-31 17:18:52 +0000

    comt

--- Assets/Entities/MovingEntities/Script/DirectionCollider.cs
+++ Assets/Entities/MovingEntities/Script/DirectionCollider.cs
@@ -13,50 +13,23 @@ public class DirectionCollider : MonoBehaviour
     [SerializeField] public float distanceMultiplier = 1;
     public List<GameObject> Inmates = new List<GameObject>();
 
-    //[SerializeField] float idleInfleunce = 0
-    [SerializeField] bool idleInfluence = false;
+    [SerializeField] bool isPlayer = false;
+    [SerializeField] int batchID = 0;
+
+    [SerializeField] InmateManager inmateManager;
+
 
     private void Awake()
     {
         if (self == null)
             self = transform.root.gameObject; // root of this child
-    }
 
-    private Collider[] InmateColliders;
+        if (TryGetComponent(out inmateManager))
+            batchID = inmateManager.inmateBatchID;
+    }
 
     void Update()
     {
-        /*
-        int count = Physics.OverlapSphereNonAlloc(transform.position, 10, InmateColliders);
-        var list = InmateColliders
-            .Take(count)
-            // remove yuourself from the list
-            .Select(collider => collider.GetComponent<MovingController>())
-            .Where(controller => controller !=)
-            .Where(controller => controller != null)
-            .Where(controller => controller.moveInput.magnitude < 0.1f && !idleInfluence)
-            ;
-
-        Vector2 direction = list
-            //.Select(controller => new Vector2(controller.transform.forward.x, controller.transform.forward.z))
-            //.Select(direction => direction * distanceFactor)
-            
-            .Select
-            (   controller =>
-                {
-                    Vector2 direction = new Vector2(controller.transform.forward.x, controller.transform.forward.z);
-                    float distance = Vector3.Distance(controller.transform.position, self.transform.position);
-                    distance = Mathf.Clamp01(distance / maxInmateDistance);
-                    float distanceFactor = 1 - distance;
-
-                    return direction * distanceFactor;
-                }
-            )
-            .Aggregate(Vector2.zero, (acc, dir) => acc + dir)
-            ;
-        direction /= list.Count();
-        */
-
         // Clean nulls (in case inmates despawn/disable)
         for (int i = Inmates.Count - 1; i >= 0; i--)
         {
@@ -71,30 +44,31 @@ public class DirectionCollider : MonoBehaviour
         }
 
         Vector2 cumulativeDirection = Vector2.zero;
+        Vector2 comulativePositon = Vector2.zero;
         for (int i = Inmates.Count - 1; i >= 0; i--)
         {
             GameObject boid = Inmates[i];
 
-            float distance = Vector3.Distance(boid.transform.position, self.transform.position);
-            distance = Mathf.Clamp01(distance / maxInmateDistance);
-            float distanceFactor = 1 - distance;
-
-            //bool isIdle = false;
-            if (boid.TryGetComponent(out MovingController mc))
+            if (boid.TryGetComponent(out InmateManager m))
             {
-                if (mc.moveInput.magnitude < 0.1f && !idleInfluence) 
+                if (m.inmateBatchID != batchID && !isPlayer)
                 {
                     Inmates.Remove(boid.gameObject);
                     continue;
                 }
             }
 
+            float distance = Vector3.Distance(boid.transform.position, self.transform.position);
+            distance = Mathf.Clamp01(distance / maxInmateDistance);
+            float distanceFactor = 1 - distance;
+
+            Vector2 position = new Vector2(boid.transform.position.x, boid.transform.position.z);
+
             Vector2 direction = new Vector2(boid.transform.forward.x, boid.transform.forward.z);
             cumulativeDirection += direction * distanceFactor;
         }
 
         Vector2 avgDirection = cumulativeDirection / Inmates.Count;
-
 
         averageDirection.Invoke(avgDirection.normalized);
     }
